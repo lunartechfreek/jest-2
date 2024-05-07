16d1c116582980025d98d63daab0b0a3025dc9cb
Daniel Turner <lunartechfreek@googlemail.com>
2024-05-07 17:14:42 +0000

    Fix lesson issue

--- scripts/game.js
+++ scripts/game.js
@@ -2,7 +2,7 @@ let game = {
     currentGame: [],
     playerMoves: [],
     score: 0,
-    choices: ["button1", "button2", "button3", "button4"]
+    choices: ["button1", "button2", "button3", "button4"],
 };
 
 function newGame() {
@@ -11,23 +11,24 @@ function newGame() {
     game.score = 0;
     showScore();
     addTurn();
-}
+};
 
 function addTurn() {
     game.playerMoves = [];
     game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
     // showTurns();
-}
+};
 
 function lightsOn(circ) {
     document.getElementById(circ).classList.add("light");
     setTimeout(function () {
         document.getElementById(circ).classList.remove("light");
     }, 400);
-}
+
+};
 
 function showScore() {
     document.getElementById("score").innerText = game.score;
-}
+};
 
 module.exports = { game, newGame, showScore, addTurn, lightsOn };
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -71,6 +71,6 @@ describe("gameplay works correctly", () => {
     test("should add correct class to light up the buttons", () => {
         let button = document.getElementById(game.currentGame[0]);
         lightsOn(game.currentGame[0]);
-        expect(button.classList).toContain(game.currentGame[0] + "light");
-    });
+        expect(button.classList).toContain("light");
+        });
 });
